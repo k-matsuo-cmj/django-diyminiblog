26a3951bd60ed8fef2a63f6942c24a76505f09db
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-03-23 20:01:24 +0900

    SQLをコンソールに出力するようにする

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -124,3 +124,20 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
